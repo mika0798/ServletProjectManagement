cb8d379683f425196f353680c2463831d1a3e484
mika0798 <192448446+mika0798@users.noreply.github.com>
2025-10-28 21:34:25 +0700

    Added decorator for jsp

--- src/main/java/com/project/filter/SiteMesh.java
+++ src/main/java/com/project/filter/SiteMesh.java
@@ -1,4 +1,9 @@
 package com.project.filter;
 
-public class SiteMesh {
+import javax.servlet.annotation.WebFilter;
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+
+@WebFilter(filterName = "sitemesh" ,urlPatterns = "/*")
+public class SiteMesh extends SiteMeshFilter{
+
 }
